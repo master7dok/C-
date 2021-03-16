1234116514bd3a5d266d4feb2fa83fe789c50db1
Master-DOK <master7dok@gmail.com>
2021-03-16 17:20:30 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std; 
+   
+int main () 
+{ 
+    int tab[n],a, i, n;
+    cin>>a;
+    for(int k=0;k<a;k++)
+    {
+    cout<<endl;
+    cin>>n;
+    for (int i = 0; i < n; i++) {   
+    cin>> tab[i]; 
+    } 
+   
+    for (int i = n-1; i >=0; i--) { 
+    cout << tab[i] <<" "; 
+    } 
+    }
+      
+    return 0; 
+} 
